3f4a60057639b96ea2043e774fb49707023064af
hac <70646122+hac01@users.noreply.github.com>
2022-08-06 21:29:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import Home from "./components/Training/Home";
+import Training from "./components/Training/Home";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
