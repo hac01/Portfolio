3ea6788ad8600b2774250327f7dd67ffd7dcebce
hac <70646122+hac01@users.noreply.github.com>
2022-08-06 20:59:57 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> AYUSH SINGH</strong>
+                <strong className="main-name"> AYUSH SINGH (Hac)</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
